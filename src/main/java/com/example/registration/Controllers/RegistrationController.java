2c1320cd983c290497cec9c340f57c9074f2b31d
Mateusz <58775549+mateusz464@users.noreply.github.com>
2024-06-18 01:35:06 +0100

    Added server side validation

--- src/main/java/com/example/registration/Controllers/RegistrationController.java
+++ src/main/java/com/example/registration/Controllers/RegistrationController.java
@@ -3,6 +3,9 @@ package com.example.registration.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Controller
 public class RegistrationController {
 
@@ -24,15 +27,29 @@ public class RegistrationController {
             @RequestParam(value = "countryCode", required = false) String countryCode,
             @RequestParam(value = "phone", required = false) String phone
     ) {
-        System.out.println("Title: " + title);
-        System.out.println("First Name: " + firstName);
-        System.out.println("Last Name: " + lastName);
-        System.out.println("Email: " + email);
-        System.out.println("Address Line 1: " + address1);
-        System.out.println("Address Line 2: " + address2);
-        System.out.println("City: " + city);
-        System.out.println("Postcode: " + postcode);
-        System.out.println("Phone Number: +" + countryCode + phone);
+        // Validation
+        String[] fieldsToValidate = {title, firstName, lastName, email, address1, postcode};
+        String[] fieldNames = {"title", "firstName", "lastName", "email", "address1", "postcode"};
+        for (int i = 0; i < fieldsToValidate.length; i++) {
+            if (fieldsToValidate[i] == null || fieldsToValidate[i].isEmpty()) {
+                // Redirect to the registration page with an error message
+                return "redirect:/registration?error=" + fieldNames[i];
+            }
+        }
+
+        // Ensure email is valid
+        String emailRegex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        if (!matcher.matches()) {
+            return "redirect:/registration?error=email";
+        }
+
+        // Remove case sensitivity from the email
+        email = email.toLowerCase();
+
+
+
         return "registration";
     }
 }
